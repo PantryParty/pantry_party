45b9abfdea734b3039852fe54aac7b263d8a3945
Brian Malinconico <bmalinconico@terminus.com>
2020-04-26 16:50:32 -0400

    Forgot stock module/

---
+++ src/app/features/stock/stock-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { Routes } from "@angular/router";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { CurrentStockComponent } from "./current-stock/current-stock.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: CurrentStockComponent
+  }
+];
+
+@NgModule({
+  imports: [NativeScriptRouterModule.forChild(routes)],
+  exports: [NativeScriptRouterModule]
+})
+export class StockRoutingModule { }
