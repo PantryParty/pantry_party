2e9331ef27675d96038e934e1d2d1bea04f719b8
arjes <arjesins@gmail.com>
2020-05-10 21:10:01 -0400

    Addingh dynamically sized ad containers (#67)
    * Addingh dynamically sized ad containers
    
    * cleanup
    
    Co-authored-by: Brian Malinconico <bmalinconico@terminus.com>

---
+++ src/app/ad-wrapper/ad-wrapper.module.ts
@@ -0,0 +1,17 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { AdWrapperComponent } from "~/app/ad-wrapper/ad-wrapper.component";
+
+@NgModule({
+  declarations: [
+    AdWrapperComponent
+  ],
+  imports: [
+    NativeScriptCommonModule
+  ],
+  schemas: [NO_ERRORS_SCHEMA],
+  exports: [
+    AdWrapperComponent
+  ]
+})
+export class AdWrapperModule { }
