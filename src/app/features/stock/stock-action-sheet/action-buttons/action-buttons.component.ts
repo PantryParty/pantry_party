32b40cc7bf0e092492518b3e8580d6b68678bb48
Brian Malinconico <bmalinconico@terminus.com>
2020-05-16 15:55:38 -0400

    Adding new action buttons for current stock

---
+++ src/app/features/stock/stock-action-sheet/action-buttons/action-buttons.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { GrocyStockEntry } from "~/app/services/grocy.interfaces";
+
+@Component({
+  selector: "ns-action-buttons",
+  templateUrl: "./action-buttons.component.html",
+  styleUrls: ["./action-buttons.component.scss"]
+})
+export class ActionButtonsComponent {
+
+  @Input() stockItem: GrocyStockEntry;
+  @Input() showMessageWhenNoActions: boolean = true;
+
+  @Output() consumeOne = new EventEmitter();
+  @Output() consumeAll = new EventEmitter();
+  @Output() openOne = new EventEmitter();
+  @Output() spoilOne = new EventEmitter();
+
+  canConsume() {
+    return this.stockItem.amount > 0;
+  }
+
+  canOpen() {
+    return this.stockItem.amount - this.stockItem.amount_opened > 0;
+  }
+
+  canPerformActions() {
+    return this.stockItem.amount > 0;
+  }
+}
---
+++ src/app/utilities/pluralize.ts
@@ -0,0 +1,3 @@
+
+export const  pluralize =
+  (count: number, singular: string, plural: string) => count > 1 ? plural : singular;
