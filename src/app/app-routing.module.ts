d064f221d5c0429f19f9a1e3dc8f62fb3ce8fa70
arjes <arjesins@gmail.com>
2020-05-16 16:01:03 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,7 @@ import { ApplicationIsSetup } from "./guards/appIsSetup";
 const routes: Routes = [
   {
     path: "",
-    // redirectTo: "/scan/purchase",
-    redirectTo: "/stock",
+    redirectTo: "/scan/purchase",
     pathMatch: "full"
   },
   {
